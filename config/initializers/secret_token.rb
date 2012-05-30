1665efe4741b9f83f6b5289c6471478e212ce86b
Stuart <stuart@Millenniums-MacBook-Pro.local>
2012-05-30 09:43:11 -0400

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FitnessSchedules::Application.config.secret_token = '06f0e4ab40a68405c6a8212bac7a1c498503b384471219beb712f1f7cfb4e5da2bb8cdf9478360099521212a971439788fa78043293f698d17141c7759030985'
