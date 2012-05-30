aa02961f583842f2d67a1b0a904718bebf7e4178
Stuart <stuart@Millenniums-MacBook-Pro.local>
2012-05-30 11:34:06 -0400

    Home Page Sketch

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,7 +4,17 @@ describe "Static Pages" do
 
   describe "Home Page" do
     before { visit root_path }
-    page.should have_selector('title', content: "Fitness Schedules")
+    
+    it "Should have proper title" do 
+      page.should have_selector('title', content: "Fitness Schedules") 
+      page.should have_selector('h1', content: "Home Page" ) 
+    end
+    
+    it "Should have the header and footer" do
+      page.should have_selector('h3', content: "footer")
+      page.should have_selector('h3', content: "header")
+    end
+              
   end
 
 end
