9f26feca4eeaf5dde7c922de34c66c3a624c2526
Stuart <sterrett@mp-sportsclub.com>
2012-05-31 10:21:45 -0400

    Client model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120530192642) do
+ActiveRecord::Schema.define(:version => 20120531141939) do
+
+  create_table "clients", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "trainers", :force => true do |t|
     t.string   "name"
