57c5bffe673f431939e9f3774ec96d5e36998b53
Stuart <sterrett@mp-sportsclub.com>
2012-06-05 09:08:28 -0400

    Signin for trainers and clients

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,4 +3,31 @@ class SessionsController < ApplicationController
 	def new
 	end
 	
+	def create
+		user = log_in_user
+		if user && user.authenticate(params[:session][:password])
+			# create new session
+			redirect_to user
+		else 
+			flash.now[:error] = "Invalid email/password combination"
+			render 'new'
+		end
+	end
+	
+	
+	
+	
+	private
+		
+		def log_in_user
+			user_type = params[:user_type]
+			if user_type == "Trainer"
+				user = Trainer.find_by_email(params[:session][:email])
+			elsif user_type == "Client"
+				user = Client.find_by_email(params[:session][:email])
+			else
+				# Error!
+				render 'new'
+			end	
+		end			
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -15,8 +15,38 @@ describe 'Authentication Pages' do
 			
 			it { should have_selector('title', text: 'Sign in') }
 			it { should have_selector('div.alert.alert-error', text: "Invalid") }
+			
+			describe 'after visiting another page' do
+				before { visit root_path }
+				
+				it { should_not have_selector('div.alert.alert-error', text: "Invalid") }
+			end
+		end
+		
+		describe 'with valid trainer information' do
+			let(:trainer) { FactoryGirl.create(:trainer) }
+			before do
+				select "Trainer", :from => "user_type"
+				fill_in "Email", 						with: trainer.email
+				fill_in "Password", 				with: trainer.password
+				click_button "Sign in"
+			end
+			
+			it { should have_selector('title', text: trainer.name) }
+		end
+		
+		describe 'with valid client information' do
+			let(:client) { FactoryGirl.create(:client) }
+			before do
+				select "Client", :from => "user_type"
+				fill_in "Email", 							with: client.email
+				fill_in "Password", 					with: client.password
+				click_button "Sign in"
+			end
+			
+			it { should have_selector('title', text: client.name) }
 		end
 	end
-
+	
 end	
 		
