d34ca3eb7b6d3b7e1b20fb8a4b73b89ab54c5f03
Stuart <shterrett@gmail.com>
2012-08-16 08:34:51 -0400

    Progress on sorting reports by Client and Trainer; scheduled_date, updated_at, and status seem to be OK. Need to polish up Client and Trainer sort and add ascending/descending toggle.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -12,14 +12,28 @@ class ReportsController < ApplicationController
   end
 
   def show
-    @trainer = Trainer.find_by_id(params[:report][:trainer_id])
+    @schedules = sorted_schedules(params) 
+  end
+  
+  private
+  
+  def sorted_schedules(params)
+    sort_key = params[:sort]
+    # sort_order = params[:sort][1]
+    params[:report][:trainer_id].blank? ? trainer_id = Trainer.pluck(:id) : trainer_id = Trainer.find_by_id(params[:report][:trainer_id]).id
     start_date = params[:report][:start_date].to_date
     end_date = params[:report][:end_date].to_date
-    @schedules = Schedule.order(params[:sort]).find(:all, :conditions => { :scheduled_date => start_date.beginning_of_day..end_date.end_of_day })
-    if @trainer
-      client_ids = @trainer.client_ids
-      @schedules = Schedule.order(params[:sort]).find(:all, :conditions => { :scheduled_date => start_date.beginning_of_day..end_date.end_of_day, :client_id => client_ids })
+    if sort_key.nil?
+      Schedule.find(:all, :conditions => { :scheduled_date => start_date.beginning_of_day..end_date.end_of_day, :trainer_id => trainer_id })
+    elsif sort_key == "trainers" || sort_key == "clients"
+      Schedule.find(:all, :include => sort_key, :order => "#{sort_key}.name", :conditions => { :scheduled_date => start_date.beginning_of_day..end_date.end_of_day, :trainer_id => trainer_id })
+    elsif sort_key == "scheduled_date" || sort_key == "rendered" || sort_key == "updated_at"
+      Schedule.find(:all, :order => sort_key, :conditions => { :scheduled_date => start_date.beginning_of_day..end_date.end_of_day, :trainer_id => trainer_id })
+    else 
+      Schedule.find(:all, :conditions => { :scheduled_date => start_date.beginning_of_day..end_date.end_of_day, :trainer_id => trainer_id })
     end
+    # If Client or Trainer, need to 'join' that table: @schedules = Schedule.find(:all, :include => :trainers, :order =>'trainers.name')
+    # desc or asc
   end
   
 end
